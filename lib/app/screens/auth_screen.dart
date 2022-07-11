2fc629b6abf24ff89a72b75b1a6631c7e6778140
al Mohamady <al.Mohamady47@gmail.com>
2022-07-11 19:31:54 +0200

    04: Auth radio button

--- lib/app/screens/auth_screen.dart
+++ lib/app/screens/auth_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_souq/components/declarations.dart';
+
+enum AuthEnum {
+  signin,
+  signup
+}
 
 class AuthScreen extends StatefulWidget {
   static const String routName = '/auth-screen';
@@ -10,8 +16,64 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  AuthEnum _auth = AuthEnum.signup;
+  final _signUpKey = GlobalKey<FormState>();
+  final _signInKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Declarations.greyBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const Text(
+                  'Welcome',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              ListTile(
+                title: const Text("New Customer", style: TextStyle(fontWeight: FontWeight.bold),),
+                leading: Radio(
+                  activeColor: Declarations.secondaryColor,
+                  value: AuthEnum.signup,
+                  groupValue: _auth,
+                  onChanged: (AuthEnum? val) {
+                    setState(() {
+                        _auth = val ?? AuthEnum.signup;
+                    });
+                  },
+                ),
+              ),
+              if (_auth == AuthEnum.signup)
+                Form(
+                    key: _signUpKey,
+                    child: Column (
+
+                    )
+                ),
+              ListTile(
+                title: const Text("Have an account ?", style: TextStyle(fontWeight: FontWeight.bold),),
+                leading: Radio(
+                  activeColor: Declarations.secondaryColor,
+                  value: AuthEnum.signin,
+                  groupValue: _auth,
+                  onChanged: (AuthEnum? val) {
+                    setState(() {
+                      _auth = val ?? AuthEnum.signin;
+                    });
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
