9f9500730ec2e1fab4b0c9a12cbb14607a6bca54
al Mohamady <al.Mohamady47@gmail.com>
2022-07-13 14:04:31 +0200

    12: Auth error handling

--- lib/app/services/auth_service.dart
+++ lib/app/services/auth_service.dart
@@ -1,25 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:my_souq/app/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:my_souq/components/declarations.dart';
+import 'package:my_souq/components/error_handling.dart';
+import 'package:my_souq/components/utils.dart';
 
 class AuthService {
 
-  void signUpUser({required String email, required String password, required String name}) async {
-
+  void signUpUser(
+      {required BuildContext context, required String email, required String password, required String name}) async {
     try {
-      User user = User(id: '', name: name, password: password, address: '', type: '', token: '', email: email);
+      User user = User(id: '',
+          name: name,
+          password: password,
+          address: '',
+          type: '',
+          token: '',
+          email: email);
 
-      http.Response res = await  http.post(
-                                            Uri.parse('$myUri01/api/signup'),
-                                            body: user.toJson(),
-                                            headers: <String, String> {
-                                              'Content-Type': 'application/json; charset=utf-8',
-                                            }
-                                        );
+      http.Response res = await http.post(
+          Uri.parse('$myUri01/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=utf-8',
+          }
+      );
+      
+      httpErrorHandel(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, "Account created, Now you are login");
+          }
+      );
     } catch (err) {
-
+        showSnackBar(context, err.toString());
     }
   }
-
-
 }
